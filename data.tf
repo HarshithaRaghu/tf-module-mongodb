4b2a75a951c34219db043d7d5589ce140fae3b39
Harshitha <harshithadarshan95@gmail.com>
2023-02-21 13:43:58 +0530

    Autopush

--- data.tf
+++ data.tf
@@ -1,8 +1,8 @@
 # Fetches the information of the remote statefile, here in our case, this will fetch the information of the VPC Statefile
+
 data "terraform_remote_state" "vpc" {
   backend = "s3"
-
-  config = {
+ config = {
         bucket = "b52-terraform-state-bucket1"
         key    = "vpc/${var.ENV}/terraform.tfstate"
         region = "us-east-1"

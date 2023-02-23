5c4361e6951d93d6589847051e645f5574aa4fb5
Harshitha <harshithadarshan95@gmail.com>
2023-02-23 10:42:55 +0530

    Autopush

--- schema.tf
+++ schema.tf
@@ -1,17 +1,17 @@
-resource "null_resource" "docdb-schema" {
+#resource "null_resource" "docdb-schema" {
   
   # This is how we can create depenency and ensure this will only run after the creation if the RDS Instance.
-  depends_on = [aws_docdb_cluster.docdb]
+  #depends_on = [aws_docdb_cluster.docdb]
 
-  provisioner "local-exec" {
-        command = <<EOF
-        cd /tmp 
-        wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
-        curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
-        unzip -o mongodb.zip
-        cd mongodb-main
-        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js 
-        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < users.js 
-        EOF
-    }  
-}
+  #provisioner "local-exec" {
+   #     command = <<EOF
+    #    cd /tmp 
+     #   wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
+      #  curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+      #  unzip -o mongodb.zip
+      #  cd mongodb-main
+      #  mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js 
+      #  mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < users.js 
+      #  EOF
+   # }  
+#}
